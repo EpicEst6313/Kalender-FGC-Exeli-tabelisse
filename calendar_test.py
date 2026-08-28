d4cc52036df8383ae1971b6a16e3ccc94c79c965
Heiki Nerut <heikinerut@gmail.com>
2026-08-28 11:36:37 +0300

    Made token controll and login funcion work properly

--- calendar_test.py
+++ calendar_test.py
@@ -17,30 +17,47 @@ from googleapiclient.discovery import build
 # Paths needed for authentication
 SECRETS_DIR = Path.home() /"GoogleCalendarSheetsSecrets" # Location of the secrets
 CREDENTIALS_FILE = SECRETS_DIR / "credentials.json" # Location of the credentials file
-TOKEN_FILE = SECRETS_DIR / "calendar_token.json" # Location of the stored authentication token (user premissions given to access stuff asked)
+TOKEN_FILE = SECRETS_DIR / "calendar_token.json" # Location of the stored authentication token (user permissions given to access stuff asked)
 
-# Scopes that say what this apps wants to do
+# Scopes that say what this apps wants access to
 SCOPES = [
     "https://www.googleapis.com/auth/calendar.readonly"
 ]
 
 
-# Controlling token existence
-if (TOKEN_FILE.exists()):
-    credentials = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+def login():
+    """
+    Function for letting user login and give permission and saving it to disk
+    """
+    flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES) # Preparing the OAuth authorization flow
+    credentials = flow.run_local_server() # Starting the local OAuth server and opening the authorization flow
+    TOKEN_FILE.write_text(credentials.to_json()) # Saving the credentials as JSON so we can reuse them next time
+    return credentials
 
-    # Controlling if the credentials are valid
-    if (credentials.valid):
-        print("tootab")
-    
-    # Requesting new ones if not valid
-    elif (credentials.expired and credentials.refresh_token):
-        credentials.refresh(Request())
-    
-    # if no refresh token then letting user login again
+def authorization_controll():
+    """
+    Function for controlling the existence of credentials and if needed and possible refreshing them
+    otherwise calling login function
+    """
+
+    # Controlling token existence
+    if (TOKEN_FILE.exists()):
+        credentials = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+
+        # Controlling if the credentials are valid
+        if (credentials.valid):
+            return credentials
+        
+        # Requesting new ones if not valid
+        elif (credentials.expired and credentials.refresh_token):
+            credentials.refresh(Request()) # Requesting tokencredentials.jsoncredentials.json refreshin if possible
+            TOKEN_FILE.write_text(credentials.to_json()) # Saving the refreshed credentials as JSON so we can reuse them next time
+            return credentials
+
+        # If couldn't refresh token then letting user login
+        else:
+            return login()
+
+    # If no token is found, letting user login
     else:
-        print("lasen kasutajal sisse logida")
-
-# if no token then letting user login
-else:
-    print("lasen kasutajal sisse logida")
+        return login()
